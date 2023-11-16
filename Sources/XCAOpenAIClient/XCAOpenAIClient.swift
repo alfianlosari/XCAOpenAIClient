f571a13c782a4de919bebc9ac31ca746cba571a3
Alfian Losari <alfianlosari@gmail.com>
2023-11-16 19:45:16 +0700

    Add generate speech method

--- Sources/XCAOpenAIClient/XCAOpenAIClient.swift
+++ Sources/XCAOpenAIClient/XCAOpenAIClient.swift
@@ -13,6 +13,35 @@ public struct OpenAIClient {
             middlewares: [AuthMiddleware(apiKey: apiKey)])
     }
     
+    public func generateSpeechFrom(input: String,
+                                   model: Components.Schemas.CreateSpeechRequest.modelPayload.Value2Payload = .tts_hyphen_1,
+                                   voice: Components.Schemas.CreateSpeechRequest.voicePayload = .alloy,
+                                   format: Components.Schemas.CreateSpeechRequest.response_formatPayload = .aac
+    ) async throws -> Data {
+        let response = try await client.createSpeech(body: .json(
+            .init(
+                model: .init(value1: nil, value2: .tts_hyphen_1),
+                input: input,
+                voice: .alloy,
+                response_format: .aac
+            )))
+        
+        switch response {
+        case .ok(let response):
+            switch response.body {
+            case .binary(let body):
+                var data = Data()
+                for try await byte in body {
+                    data.append(contentsOf: byte)
+                }
+                return data
+            }
+            
+        case .undocumented(let statusCode, let payload):
+            throw "OpenAIClientError - statuscode: \(statusCode), \(payload)"
+        }
+    }
+    
     public func generateDallE3Image(prompt: String,
                                     quality: Components.Schemas.CreateImageRequest.qualityPayload = .standard,
                                     responseFormat: Components.Schemas.CreateImageRequest.response_formatPayload = .url,
